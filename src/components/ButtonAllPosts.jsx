4980de912e206b4dc7ab6b8217bc797ce8686eb9
clovis-rosa <clovis.souzarosa@gmail.com>
2021-02-03 15:21:37 +0000

    Testing with button styles

--- src/components/ButtonAllPosts.jsx
+++ src/components/ButtonAllPosts.jsx
@@ -5,18 +5,14 @@ export default function ButtonAllPosts() {
   return (
     <ButtonSection>
       <ButtonContainer>
-        <Button>
-          <div class="btn-bg">
-            <div>Read More</div>
-          </div>
-        </Button>
+        <Button>Hover over me</Button>
       </ButtonContainer>
     </ButtonSection>
   )
 }
 
 const ButtonSection = styled.section`
-  background: var(--color-black);
+  /* background: var(--color-black); */
   padding: 0 2rem 3rem;
 `
 
@@ -27,67 +23,43 @@ const ButtonContainer = styled.div`
   display: grid;
   place-content: center;
 `
-const Button = styled.a`
-  /* position: relative; */
-  max-width: 100%;
-  display: flex;
-  margin-right: 0.5rem;
-  margin-bottom: 1rem;
-  margin-left: 0.5rem;
-  padding: 0.5rem 1rem;
-  justify-content: center;
-  align-items: center;
-  border-radius: 0.05rem;
-  background-color: var(--color-black);
-  box-shadow: 0 0 0 1px var(--color-white);
+const Button = styled.button`
   font-family: var(--font-family-primary);
-  color: var(--color-white);
-  font-size: 0.7rem;
-  line-height: 1em;
-  font-weight: 500;
-  text-align: center;
-  letter-spacing: 0.05em;
-  text-decoration: none;
-  text-transform: uppercase;
+  background: var(--color-white);
+  color: var(--color-black);
+  cursor: pointer;
+  font-size: 1rem;
+  padding: 0.8rem;
+  border: 3px solid var(--color-black);
+  transition: all 0.5s;
+  /* border-radius: 10px; */
+  width: auto;
+  position: relative;
 
-  .btn-bg {
-    width: 100%;
-    height: 100%;
-    border-radius: 0.05rem;
-    background-color: var(--color-black);
-    box-shadow: 0 0 0 1px var(--color-white);
-
-    font-family: var(--font-family-primary);
-    color: var(--color-white);
-    font-size: 0.7rem;
-    line-height: 1em;
-    font-weight: 500;
-    text-align: center;
-    letter-spacing: 0.05em;
-    text-transform: uppercase;
-    transform: translate(0.5em, -0.5em);
-
-    &:hover {
-      transform: translate(0.25em, -0.25em);
-      transition: transform 100ms ease-out;
-    }
-  }
-
-  div {
-    padding: 0.5rem 1rem;
-    border-radius: 0.05rem;
-    transition: transform 150ms ease-out;
-
-    &:hover {
-      /* transform: translate(0.25em, -0.25em);
-      transition: transform 100ms ease-out; */
-    }
+  &::after {
+    content: "❯";
+    font-weight: 400;
+    position: absolute;
+    left: 85%;
+    top: 23%; /*31%*/
+    right: 5%;
+    bottom: 0;
+    opacity: 0;
   }
 
   &:hover {
-    top: auto;
-    outline: 0;
-    /* transform: translate(0.25em, -0.25em);
-    transition: transform 100ms ease-out; */
+    background: var(--color-white);
+    color: var(--color-black);
+    border: 3px solid var(--color-black);
+
+    transition: all 0.5s;
+    /* border-radius: 10px; */
+    box-shadow: 0px 6px 15px var(--color-black);
+    padding: 1rem 3.5rem 1rem 1.5rem;
+
+    &::after {
+      opacity: 1;
+      transition: all 0.5s;
+    }
   }
 `
