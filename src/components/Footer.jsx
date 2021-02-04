f0c72ce4a0f60bc4ff60bb83f08aa8f0bfc32a9f
clovis-rosa <clovis.souzarosa@gmail.com>
2021-02-04 23:23:40 +0000

    Fixing query error in Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,130 +1,61 @@
 import React from "react"
 import { Link } from "gatsby"
 import styled from "styled-components"
+import { UseFooterDataQuery } from "../hooks/UseFooterDataQuery"
 
 export default function Footer() {
+  const getAllFooterList = () => {
+    const allFooterList = []
+    const allFooter = UseFooterDataQuery()
+    allFooter.allDataJson.edges.forEach(footerEdge => {
+      allFooterList.push({
+        id: footerEdge.node.id,
+        title: footerEdge.node.title,
+        linkName: footerEdge.node.url.linkName,
+        linkUrl: footerEdge.node.url.linkUrl,
+        linkId: footerEdge.node.url.id,
+      })
+    })
+    return allFooterList
+  }
+
+  const allFooterList = getAllFooterList()
+
+  console.log(UseFooterDataQuery())
+  console.log(allFooterList)
+
   return (
-    <>
-      <FooterSection>
-        <FooterContainer>
-          <FooterWrap>
-            <h3>Products</h3>
-            <ul>
-              <li>
-                <Link to="/">Something</Link>
-              </li>
-              <li>
-                <Link to="/">Enterprise</Link>
-              </li>
-              <li>
-                <Link to="/">Pricing</Link>
-              </li>
-              <li>
-                <Link to="/">Customer Stories</Link>
-              </li>
-              <li>
-                <Link to="/">Release Notes</Link>
-              </li>
-              <li>
-                <Link to="/">Status</Link>
-              </li>
-              <li>
-                <Link to="/">Download</Link>
-              </li>
-            </ul>
-          </FooterWrap>
-          <FooterWrap>
-            <h3>Features</h3>
-            <ul>
-              <li>
-                <Link to="/">Version Control</Link>
-              </li>
-              <li>
-                <Link to="/">Design Collaboration</Link>
-              </li>
-              <li>
-                <Link to="/">Developer Handoff</Link>
-              </li>
-            </ul>
-          </FooterWrap>
-          <FooterWrap>
-            <h3>Resources</h3>
-            <ul>
-              <li>
-                <Link to="/">Getting Started</Link>
-              </li>
-              <li>
-                <Link to="/">Blog</Link>
-              </li>
-              <li>
-                <Link to="/">Books</Link>
-              </li>
-              <li>
-                <Link to="/">Webinars</Link>
-              </li>
-              <li>
-                <Link to="/">Design Systems</Link>
-              </li>
-              <li>
-                <Link to="/">Remote Work</Link>
-              </li>
-              <li>
-                <Link to="/">Help Center</Link>
-              </li>
-            </ul>
-          </FooterWrap>
-          <FooterWrap>
-            <h3>Community</h3>
-            <ul>
-              <li>
-                <Link to="/">Twitter</Link>
-              </li>
-              <li>
-                <Link to="/">LinkeIn</Link>
-              </li>
-              <li>
-                <Link to="/">Facebook</Link>
-              </li>
-              <li>
-                <Link to="/">Dribbble</Link>
-              </li>
-              <li>
-                <Link to="/">Instagram</Link>
-              </li>
-            </ul>
-          </FooterWrap>
-          <FooterWrap>
-            <h3>Company</h3>
-            <ul>
-              <li>
-                <Link to="/">About</Link>
-              </li>
-              <li>
-                <Link to="/">Careers</Link>
-              </li>
-              <li>
-                <Link to="/">Legal</Link>
-              </li>
-            </ul>
-          </FooterWrap>
-        </FooterContainer>
-        <div>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </div>
-      </FooterSection>
-    </>
+    <FooterSection>
+      <FooterContainer>
+        {allFooterList.map(data => {
+          return (
+            <div key={data.id}>
+              <h3>{data.title}</h3>
+              <ul>
+                <li>
+                  <Link to={`/${data.linkUrl}`}>{data.linkName}</Link>
+                </li>
+              </ul>
+            </div>
+          )
+        })}
+      </FooterContainer>
+      <div>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.com">Gatsby</a>
+      </div>
+    </FooterSection>
   )
 }
 
 const FooterSection = styled.footer`
   background: var(--color-black);
   color: var(--color-white);
-  padding: 3rem 2rem 6rem;
+  padding: 6rem 2rem 3rem;
 
   @media screen and (max-width: 801px) {
-    padding: 3rem 2rem 6rem;
+    padding: 6rem 2rem 3rem;
   }
 `
 const FooterContainer = styled.nav`
@@ -132,62 +63,7 @@ const FooterContainer = styled.nav`
   max-width: 1160px;
   margin: 0 auto;
 
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  /* align-items: center; */
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
   gap: 50px;
-`
-const FooterWrap = styled.div`
-  padding: 0 1rem 2rem 1rem;
-  font-family: var(--font-family-secondary-mono);
-  color: var(--color-white);
-
-  h3 {
-    font-family: var(--font-family-secondary-mono);
-    font-size: 0.6rem;
-    font-weight: 700;
-    line-height: 1.5;
-    letter-spacing: 0.05em;
-    text-transform: uppercase;
-    margin-bottom: 1rem;
-  }
-
-  ul {
-    li {
-      list-style: none;
-
-      a {
-        position: relative;
-        font-family: var(--font-family-secondary-mono);
-        font-size: 0.6rem;
-        font-weight: 400;
-        line-height: 1.5;
-        letter-spacing: 0.05em;
-        cursor: pointer;
-        margin-bottom: 1rem;
-        text-decoration: none;
-        color: inherit;
-
-        &::after {
-          position: absolute;
-          top: 100%;
-          left: 0;
-          width: 100%;
-          height: 2px;
-          background: var(--color-white);
-          content: "";
-          opacity: 0;
-          transition: opacity 0.3s, transform 0.3s;
-          transform: translateY(10px);
-        }
-
-        &:hover::after,
-        &:focus::after {
-          opacity: 1;
-          transform: translateY(0px);
-        }
-      }
-    }
-  }
 `
