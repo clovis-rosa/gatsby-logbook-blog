edd6d35bf2b62fc806c81b4efa01443149bb5634
clovis-rosa <clovis.souzarosa@gmail.com>
2021-02-06 00:37:40 +0000

    Working on blog post page styles

--- src/templates/BlogPostLayout.js
+++ src/templates/BlogPostLayout.js
@@ -2,66 +2,80 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 import Bio from "../components/Bio"
 import SEO from "../components/Seo"
+import styled from "styled-components"
 
-const BlogPostTemplate = ({ data }) => {
+export default function BlogPostTemplate({ data }) {
   const post = data.markdownRemark
   const { previous, next } = data
+  // console.log(post, `==========> POST`)
 
   return (
-    <main>
+    <>
       <SEO
         title={post.frontmatter.title}
         description={post.frontmatter.description || post.excerpt}
       />
-      <article
-        className="blog-post"
-        itemScope
-        itemType="http://schema.org/Article"
-      >
-        <header>
-          <h1 itemProp="headline">{post.frontmatter.title}</h1>
-          <p>{post.frontmatter.date}</p>
-        </header>
-        <section
-          dangerouslySetInnerHTML={{ __html: post.html }}
-          itemProp="articleBody"
-        />
-        <hr />
-        <footer>
-          <Bio />
-        </footer>
-      </article>
-      <nav className="blog-post-nav">
-        <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={`/blog${previous.fields.slug}`} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={`/blog${next.fields.slug}`} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
-      </nav>
-    </main>
+      <BlogPostStyles>
+        <article>
+          <BlogPostHeader>
+            <h1>{post.frontmatter.title}</h1>
+            <h3>{post.frontmatter.description}</h3>
+            <small>{post.frontmatter.date}</small>
+          </BlogPostHeader>
+          <section dangerouslySetInnerHTML={{ __html: post.html }} />
+          <hr />
+          <footer>
+            <Bio />
+          </footer>
+        </article>
+        <nav>
+          <ul
+            style={{
+              display: `flex`,
+              flexWrap: `wrap`,
+              justifyContent: `space-between`,
+              listStyle: `none`,
+              padding: 0,
+            }}
+          >
+            <li>
+              {previous && (
+                <Link to={`/blog${previous.fields.slug}`} rel="prev">
+                  ← {previous.frontmatter.title}
+                </Link>
+              )}
+            </li>
+            <li>
+              {next && (
+                <Link to={`/blog${next.fields.slug}`} rel="next">
+                  {next.frontmatter.title} →
+                </Link>
+              )}
+            </li>
+          </ul>
+        </nav>
+      </BlogPostStyles>
+    </>
   )
 }
 
-export default BlogPostTemplate
+const BlogPostStyles = styled.main`
+  padding: 6rem 2rem 3rem;
+`
+
+const BlogPostHeader = styled.header`
+  max-width: 1160px;
+  margin: 0 auto;
+
+  h1 {
+    font-family: var(--font-family-primary);
+    font-size: 3.75rem;
+    font-weight: 400;
+    line-height: 1.1;
+    letter-spacing: normal;
+    margin-bottom: 0;
+  }
+`
 
 export const pageQuery = graphql`
   query BlogPostBySlug(
--- src/templates/Layout.js
+++ src/templates/Layout.js
@@ -2,13 +2,14 @@ import React from "react"
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
 import Hero from "../components/Hero"
+import BlogPostLayout from "../templates/BlogPostLayout"
 
 export default function Layout({ children }) {
   return (
     <>
       <header>
         <Navbar />
-        <Hero />
+        {!BlogPostLayout ? <Hero /> : null}
       </header>
 
       {children}
